e1c41f25b2c9dc86d9cf06fecacd612689d0d18c
Anwar Ullah <anwarullah8389@gmail.com>
2026-07-09 11:32:33 +0500

    Update 1.01_Grocery_Bill_Variables.js

--- Basic_Javasript/1.01_Grocery_Bill_Variables.js
+++ Basic_Javasript/1.01_Grocery_Bill_Variables.js
@@ -1,1 +1,14 @@
 
+let itemPrice = 220;
+let itemQauntity = 5;
+
+const taxRate = (0.08);
+
+let subTotal = (itemPrice * itemQauntity);
+let texAmmount = (subTotal * taxRate)
+let total = (subTotal + texAmmount);
+
+
+console.log("Sub-Total     $" + subTotal.toFixed(2));
+console.log("Tax Ammount   $" + texAmmount.toFixed(2))
+console.log("Total         $" + total.toFixed(2));
