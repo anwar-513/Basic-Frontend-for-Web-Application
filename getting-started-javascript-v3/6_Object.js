264f6f3d2c56b7ea4937f428859b08112817710d
anwar-513 <anwarullah8389@gmail.com>
2026-08-15 03:15:00 +0500

    object

---
+++ getting-started-javascript-v3/6_Object.js
@@ -0,0 +1,13 @@
+const book = {
+    title: "Getting Started with JavaScript",
+    author: {
+        firstName: "Kyle",
+        secondName: "Cook"
+    },
+    yearOfPublish: 2025,
+    publish() {
+        console.log("The book is publishing");
+    }
+};
+
+book.publish(); // The book is publishing
